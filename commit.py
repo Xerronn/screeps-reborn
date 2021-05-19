6d70ec448a6e6e39c6b3451c3ed09686930ae9e5
Xerronn <zonisick@gmail.com>
2021-05-19 11:47:34 -0400

    Wrote a simple python script that handles the pushing of a hierarchical folder structure to screeps servers. It first flattens the files into a single directory, then modifies the require statements to compensate

---
+++ commit.py
@@ -0,0 +1,78 @@
+import os, re, shutil, json, requests
+from requests.auth import HTTPBasicAuth
+
+#load the credentials from the config file
+with open("config/credentials.json") as f:
+    credentials = json.load(f)
+
+url = "https://screeps.com:443/api/user/code"
+
+#define that we are sending json files
+headers = {
+    'Content-Type': 'application/json; charset=utf-8'
+}
+
+data = {
+    "branch": "default",
+    "modules": {}
+}
+
+fileList = []
+for subdir, dirs, files in os.walk("./src"):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        if filepath.endswith(".js"):
+            fileList.append(filepath.replace("src_", ""))
+
+#remove old files and remake the directory
+shutil.rmtree("./dist")
+os.mkdir("./dist")
+
+#copy all the files we found in the src folder to the dist folder
+for file in fileList:
+    new = file.replace(os.sep, "_").replace("src_", "")
+    shutil.copy(file, f"./dist/{new}")
+
+#now we iterate through the files and modify import statements to match the flat names
+for subdir, dirs, files in os.walk("./dist"):
+    for file in files:
+        filepath = subdir + os.sep + file
+        with open(filepath) as infile:
+            contents = infile.read()
+            #find all require statements
+            requires = ", ".join(re.findall('require\(.+\)', contents))
+            #get the contents inside the parenthesis
+            paths = re.findall('\(([^)]+)\)', requires)
+            for path in paths:
+                new = path
+                if "./" in new[0:3]:
+                    directory = file.split("_")
+                    #make sure its not root
+                    if len(directory) > 1:
+                        new = new.replace("./", directory[0] + "/")
+                    else:
+                        new = new.replace("./", "")
+                if "../" in new[0:4]:
+                    new = new.replace("../", "")
+                new = new.replace("/", "_")
+
+                contents = contents.replace(path, new)
+        
+        #replace the contents of the file with the edited requires
+        with open(filepath, 'w') as outfile:
+            outfile.write(contents)
+        
+        data["modules"][filepath.split('\\')[-1][:-3]] = contents
+            
+
+
+
+r = requests.post(
+    url = url, 
+    data = json.dumps(data, separators=(',', ':')), 
+    headers = headers, 
+    auth = HTTPBasicAuth(credentials['username'], credentials['password'])
+)
+
+print(r.content)
