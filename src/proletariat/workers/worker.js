41ed534e2d0b795b34b5eeebf92ef9346ea5ff49
Xerronn <zonisick@gmail.com>
2021-07-21 22:12:00 -0400

    added comments

--- src/proletariat/workers/worker.js
+++ src/proletariat/workers/worker.js
@@ -56,6 +56,9 @@ class Worker extends Proletarian {
         }
     }
 
+    /**
+     * upgrade the controller
+     */
     upgradeController() {
         let controller = Game.rooms[this.room].controller;
 
@@ -66,7 +69,10 @@ class Worker extends Proletarian {
         }
     }
 
-    buildNearest() {
+    /**
+     * build construction sites closest to current position
+     */
+    build() {
         let liveClosestSite = Game.getObjectById(this.memory.closestSite);
         
         if (!liveClosestSite) {
