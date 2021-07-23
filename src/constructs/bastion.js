c29dae30610ef245bd68e64a6713649c20256ea4
Xerronn <zonisick@gmail.com>
2021-07-23 12:04:44 -0400

    More complex repairing method

--- src/constructs/bastion.js
+++ src/constructs/bastion.js
@@ -5,12 +5,18 @@ class Bastion extends Construct {
     constructor(towerId) {
         super(towerId);
 
+        this.repairTargets = [];
     }
 
     run() {
         //set tower filled flag
         if (this.store.getFreeCapacity(RESOURCE_ENERGY) > this.store.getCapacity(RESOURCE_ENERGY) / 4) {
             global.Archivist.setTowersFilled(this.room, false);
+        }
+
+        //find new repair targets every 15 ticks
+        if (Game.time % 15) {
+            this.findRepairTargets();
         }
 
         if (!this.simpleAttack()) {
@@ -33,20 +39,39 @@ class Bastion extends Construct {
     }
 
     repair() {
+        let target = undefined;
+        let tempTargets = [...this.repairTargets];
+
+        let index = 0;
+        for (let id of tempTargets) {
+            let liveObj = Game.getObjectById(id)
+            if (liveObj.hits < liveObj.hitsMax) {
+                target = liveObj;
+                break;
+            } else {
+                this.repairTargets.splice(index, 1);
+                index++;
+            }
+        }
+
+        if (target && this.store.getUsedCapacity(RESOURCE_ENERGY) > 250) {
+            this.liveObj.repair(target);
+        }
+    }
+
+    findRepairTargets() {
+        //erase old repairTargets
+        this.repairTargets = [];
+
         let roads = global.Archivist.getStructures(this.room, STRUCTURE_ROAD);
         let containers = global.Archivist.getStructures(this.room, STRUCTURE_CONTAINER);
         
         let repairables = roads.concat(containers).filter(
             obj => obj.hits < obj.hitsMax
         );
-        
-        //might be a bit cpu intensive, shouldn't be really necessary for now
-        //let target =  _.sortBy(repairables, (struc) => struc.hits/struc.hitsMax)[0];
-        let target = repairables[0];
 
-        if (target && this.store.getUsedCapacity(RESOURCE_ENERGY) > 250) {
-            this.liveObj.repair(target);
-        }
+        let sortedRepairables = _.sortBy(repairables, (struc) => struc.hits/struc.hitsMax).map(obj => obj.id);
+        this.repairTargets = sortedRepairables;
     }
 }
 
