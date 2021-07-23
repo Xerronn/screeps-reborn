9a6de44b8cc216684027f88e3499daf08ba99f82
Xerronn <zonisick@gmail.com>
2021-07-23 13:29:04 -0400

    Simple filler implementation

---
+++ src/proletariat/workers/runner.js
@@ -0,0 +1,21 @@
+const Worker = require("./worker");
+
+//creep that fills up extensions and towers
+class Runner extends Worker {
+    constructor(creepId) {
+        super(creepId)
+    }
+
+    run() {
+        let freeCapacity = this.store.getFreeCapacity(RESOURCE_ENERGY);
+        if (freeCapacity > 0) {
+            this.withdrawStorage();
+        } else if (!global.Archivist.getTowersFilled(this.room)) {
+            this.fillTowers();
+        } else if (!global.Archivist.getExtensionsFilled(this.room)) {
+            this.fillExtensions();
+        }
+    }
+}
+
+module.exports = Runner
