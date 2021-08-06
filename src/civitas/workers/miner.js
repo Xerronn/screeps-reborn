291c4f781a6d37bd2a0d470cdca54055bed76642
Xerronn <zonisick@gmail.com>
2021-08-06 03:57:53 -0400

    Added logic so that the miner can request a replacement before dying, so that that after spawning and travel time, the new miner will arrive just in time to replace the old one

--- src/civitas/workers/miner.js
+++ src/civitas/workers/miner.js
@@ -11,10 +11,12 @@ class Miner extends Worker {
             this.containerId = this.memory.container;
         } else {
             let allContainers = global.Archivist.getStructures(this.room, STRUCTURE_CONTAINER);
-            let container = this.source.pos.findClosestByRange(allContainers);
+            let container = this.source.pos.findInRange(allContainers, 1)[0];
 
-            this.containerId = container.id;
-            this.memory.container = container.id;
+            if (container) {
+                this.containerId = container.id;
+                this.memory.container = container.id;
+            }
         }
 
         //if a source link exists
@@ -22,6 +24,12 @@ class Miner extends Worker {
         if (sourceLink) {
             this.linkId = sourceLink; 
         }
+
+        //calculate when to spawn a new miner
+        let distanceToSource = Game.rooms[this.room].find(FIND_MY_SPAWNS)[0].pos.findPathTo(this.source).length;
+        let spawnTime = this.body.length * 3;
+        this.timeToSpawn = distanceToSource + spawnTime;
+        this.replaced = false;
         
         this.update(true);
     }
@@ -35,7 +43,7 @@ class Miner extends Worker {
             //attributes that will change tick to tick
             this.container = Game.getObjectById(this.containerId);
 
-            //keep checking to see if a link exists every 25 seconds
+            //keep checking to see if a link exists every 25 ticks
             if (this.linkId) {
                 this.link = Game.getObjectById(this.linkId);
             } else if (Game.time % 25 == 0) {
@@ -68,11 +76,25 @@ class Miner extends Worker {
                     this.build();
                 }
             }
-        } 
+        }
+
+        //make sure to spawn new miner before the current one dies, to maintain 100% uptime
+        if (!this.replaced && this.ticksToLive <= this.timeToSpawn) {
+            //basically rebirth but without the dying first
+            this.getSupervisor().initiate({
+                'body': [...this.body],
+                'type': this.memory.type,
+                'memory': {...this.memory}
+            });
+
+            //no more rebirth for you
+            delete this.memory.generation;
+            this.replaced = true;
+        }
     }
 
     /**
-     * Overriden harvest method that moves to container instead of to source
+     * Overridden harvest method that moves to container instead of to source
      */
     //TODO: add automatic container repairing too
     harvest() {
@@ -92,6 +114,14 @@ class Miner extends Worker {
         } else {
             this.liveObj.moveTo(this.link);
         }
+    }
+
+    /**
+     * Method to spawn the successor creep that will arrive 
+     * at the source right when the current miner dies
+     */
+    spawnSuccessor() {
+
     }
 }
 
