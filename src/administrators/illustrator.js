562d0369edc4ebf5e5e8bfce1232c6d552b38f02
Xerronn <zonisick@gmail.com>
2021-08-26 19:38:51 -0400

    Messing around with css and html in console, managed to make a circle

--- src/administrators/illustrator.js
+++ src/administrators/illustrator.js
@@ -9,6 +9,12 @@ class Illustrator {
     illustrate() {
         //todo: map and room visuals
     }
+
+    circle() {
+        let result = `<style> #circle {width: 250px; height: 250px; -webkit-border-radius: 125px; -moz-border-radius: 125px; border-radius: 125px; background: red;} </style>`;
+        result += `<div id="circle"></div>`;
+        console.log(result);
+    }
 }
 
 module.exports = Illustrator;
