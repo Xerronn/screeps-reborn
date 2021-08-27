5768197f3947460148cecf0a230cd9ce5dbc92e9
Xerronn <zonisick@gmail.com>
2021-08-27 14:22:10 -0400

    Workshops are now classified as either a product or reagent lab. product labs take from the reagent labs to make new chemicals, reagent labs do nothing

--- src/castrum/workshop.js
+++ src/castrum/workshop.js
@@ -6,39 +6,79 @@ class Workshop extends Castrum {
         super(labId);
 
         let anchor = global.Archivist.getAnchor(this.room);
-        this.type = classifyLab({
+        this.type = this.classify({
             "x": this.pos.x - anchor.x,
             "y": this.pos.y - anchor.y
         });
+
+        this.reagents = this.getSupervisor().reagentWorkshops;
+        this.mineralCount = undefined;
+
+        this.update(true);
     }
-}
 
-/**
- * Function that takes anchor coordinates and calculates what type of lab it is
- * @param {Object} roomPosition 
- * @returns Lab classification
- */
-function classifyLab(roomPosition) {
-    let simple = [{"x":8,"y":7},{"x":7,"y":8}];
-    let medium = [{"x":9,"y":7}, {"x":7,"y":9}];
-    let advanced = [{"x":9,"y":8}, {"x":8,"y":9}];
-    let complex = [{"x":10,"y":8}, {"x":8,"y":10}];
-    let product = [{"x":10,"y":9},{"x":9,"y":10}];
+    update(force=false) {
+        if (this.updateTick != Game.time || force == true) {
+            if (!super.update(force)) return false;
+            //attributes that can change tick to tick
+            this.cooldown = this.liveObj.cooldown;
+            //default to hydrogen just so this.mineralCount doesn't check for energy levels
+            this.mineralType = this.liveObj.mineralType || RESOURCE_HYDROGEN;
+            if (this.store) this.mineralCount = this.store.getUsedCapacity(this.mineralType);
+        }
+        return true;
+    }
 
-    let types = {
-        "simple": simple, 
-        "medium": medium, 
-        "advanced": advanced, 
-        "complex": complex, 
-        "product": product
-    };
+    run() {
+        switch(this.type) {
+            case "reagent":
+                //do nothing?
+                
+                break;
+            //everything else
+            case "product":
+                if (!this.store && this.mineralCount > 2000) {
+                    global.Archivist.setLabsFilled(this.room, true);
+                }
+                if (this.getReagentsReady() && this.cooldown == 0) {
+                    try {
+                        this.liveObj.runReaction(this.reagents[0].liveObj, this.reagents[1].liveObj);
+                    } catch (err) {
 
-    for (let type in types) {
-        for (let coords of types[type]) {
-            if (coords.x == roomPosition.x && coords.y == roomPosition.y) {
-                return type;
+                    }
+                    this.getSupervisor().productWorkshops
+                }
+                break;
+        }
+    }
+
+    /**
+     * Method that returns if the reagents are ready to react
+     */
+    getReagentsReady() {
+        for (let workshop of this.reagents) {
+            if (workshop.cooldown !== 0 || workshop.mineralCount === 0) {
+                return false;
             }
         }
+        return true;
+    }
+
+    /**
+     * Method that takes anchor coordinates and calculates what type of lab it is
+     * @param {Object} roomPosition 
+     * @returns Lab classification
+     */
+    classify(roomPosition) {
+        let reagents = [{"x":9,"y":8}, {"x":8,"y":9}];
+        for (let coords of reagents) {
+            if (coords.x == roomPosition.x && coords.y == roomPosition.y) {
+                this.getSupervisor().reagentWorkshops.push(this);
+                return "reagent";
+            }
+        }
+        this.getSupervisor().productWorkshops.push(this);
+        return "product";
     }
 }
 
